af85b64f24d529a209b159235fa99326cdb2f1ba
dolar <dolarsingh111@gmail.com>
2021-04-29 18:00:05 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyCharts from './components/chart_page'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1>DynamoDb Cupcake Count Data</h1>
+            <h3>Using Go Lang To Get DynamoDb Data</h3> 
+            <MyCharts/>
     </div>
   );
 }
